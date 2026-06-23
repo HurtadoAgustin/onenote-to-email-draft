5b4fc9a69e375c7c05c8f6db10ada28f9f1b8d31
Agustín Hurtado <leandroagustinhurtado@gmail.com>
2026-06-23 15:14:55 -0300

    modificación de templates

--- src/templates/mails/completedInQA.ts
+++ src/templates/mails/completedInQA.ts
@@ -17,12 +17,19 @@ export const completedQaTemplate: EmailTemplate = {
     Este story fue desarrollado y probado internamente en base a las siguientes condiciones:.
   </p>
 
+  <h2 style="color: #1f4e79; font-size: 18px; font-weight: 400; margin: 18px 0 8px 0;">Descripción</h2>
+  <ul>
+    <li>{{descripcion}}</li>
+  </ul>
+
   <h2 style="color: #1f4e79; font-size: 18px; font-weight: 400; margin: 18px 0 8px 0;">Condiciones de satisfacción</h2>
+
+  <p style="margin: 10px 0 4px 36px;"><strong>Cambios de comportamiento</strong></p>
   <ul style="margin-left: 36px;">
     {{cambios}}
   </ul>
 
-  <h2 style="color: #1f4e79; font-size: 18px; font-weight: 400; margin: 18px 0 8px 0;">Condiciones de Integración con el ERP</h2>
+  <p style="margin: 10px 0 4px 36px;"><strong>Condiciones de Integración con el ERP</strong></p>
   <ul style="margin-left: 36px;">
     {{integracion}}
   </ul>
@@ -36,8 +43,7 @@ export const completedQaTemplate: EmailTemplate = {
   <h2 style="color: #1f4e79; font-size: 18px; font-weight: 400; margin: 18px 0 8px 0;">Pruebas recomendadas:</h2>
   <ul></ul>
 
-  <p>Quedo atento a cualquier comentario.</p>
-
+  <p>Quedo atento a cualquier comentario.<br />Saludos.</p>
 </div>
   `,
   fieldMappings: changeOrderFieldMappings,
